971823128a8090cdd25c618aecedd6b29315e9a6
gauri <gauritawale042gmail.com>
2022-10-30 08:05:41 +0530

    Added shift method is here

---
+++ shift.js
@@ -0,0 +1,10 @@
+const arr=["shreya","Varun","Pratha","Krish","Arjun","Raj"]
+//shift() method is used to remove strarting (1st) element...
+console.log(arr)
+arr.shift()
+console.log(arr)
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node shift.js
+//[ 'shreya', 'Varun', 'Pratha', 'Krish', 'Arjun', 'Raj' ]
+//[ 'Varun', 'Pratha', 'Krish', 'Arjun', 'Raj' ]
