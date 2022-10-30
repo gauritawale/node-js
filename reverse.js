7d6b252017d250d08faecb507066d765e4b044c6
gauri <gauritawale042gmail.com>
2022-10-30 07:16:21 +0530

    Create the reverse method

---
+++ reverse.js
@@ -0,0 +1,9 @@
+const arr = ["satya","Dhiraj","vaibhavi","Avinash"]
+console.log(arr)
+arr.reverse()                     //if we want to reverse the array then revers() method is used..
+console.log(arr)
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node reverse.js 
+//[ 'satya', 'Dhiraj', 'vaibhavi', 'Avinash' ]
+//[ 'Avinash', 'vaibhavi', 'Dhiraj', 'satya' ]
