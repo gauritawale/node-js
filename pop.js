4e4126f7c5d33d944d7cc3eaa4c45b676defe486
gauri <gauritawale042gmail.com>
2022-10-30 07:11:19 +0530

    create the pop method

---
+++ pop.js
@@ -0,0 +1,7 @@
+const arr = ["Rihan","Ruhani","Anu","Anurag","Gauri"]
+arr.pop()                           //pop() method used to removes last element from an array
+console.log(arr)
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node pop.js
+//[ 'Rihan', 'Ruhani', 'Anu', 'Anurag' ]
