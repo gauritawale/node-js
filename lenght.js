801c16861e85284d45f45beaef24ea7449cb5b17
gauri <gauritawale042gmail.com>
2022-10-30 07:07:10 +0530

    Create length method

---
+++ lenght.js
@@ -0,0 +1,7 @@
+const arr=["Prathana","Anmol", "Vinayak"]
+const arrLength = arr.length                //length is not method it is a proprty that counts total number of elements in array..
+console.log(arrLength)
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node lenght.js
+3
