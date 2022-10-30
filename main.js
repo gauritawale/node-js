04b1f825b87ca8a5d66ead901b53b744041f916a
gauri <gauritawale042gmail.com>
2022-10-30 06:54:33 +0530

    create push method on array

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('Hello Student')
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node main.js
+//Hello Student
---
+++ push.js
@@ -0,0 +1,16 @@
+const arr = [ ]
+arr.push("Gauri")       //push() method adds elements form the end  (right side)
+arr.push("pooja")
+console.log(arr)
+arr.push("sakshi")
+arr.push("pratiksha")
+console.log(arr)
+arr.push("rutika")
+arr.push("priya")
+console.log(arr)
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node push.js
+//[ 'Gauri', 'pooja' ]
+//[ 'Gauri', 'pooja', 'sakshi', 'pratiksha' ]
+//[ 'Gauri', 'pooja', 'sakshi', 'pratiksha', 'rutika', 'priya' ]
