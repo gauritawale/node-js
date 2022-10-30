8f9b8cdb2c95cb4ac4e762d8368ca919538d297d
gauri <gauritawale042gmail.com>
2022-10-30 07:00:58 +0530

    Create join method

---
+++ join.js
@@ -0,0 +1,9 @@
+const arr = ["Gauri","mayur","anmol","ram","akshay"]
+console.log(arr)
+console.log(arr.join("@"))      //join() method is similar to toString() method but we can specify seprator(space, doller $, hash #)
+
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node join.js
+[// 'Gauri', 'mayur', 'anmol', 'ram', 'akshay' ]
+//Gauri@mayur@anmol@ram@akshay
