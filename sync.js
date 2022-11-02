a02bd8cba3c23c205fa096bc79c1c78caf4bdbe3
gauri <gauritawale042gmail.com>
2022-11-02 07:23:16 +0530

    Create sync method

---
+++ sync.js
@@ -0,0 +1,13 @@
+console.log("person 1")
+console.log("person 2")
+console.log("person 3")
+console.log("person 4")
+console.log("person 5")
+
+
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node synce.js
+//person 1
+//person 2
+//person 3
+//person 4
+//person 5
