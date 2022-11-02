12aaac9c7deb67bf1b9b6b7dcf5d3bec43abf662
gauri <gauritawale042gmail.com>
2022-11-02 10:30:00 +0530

    Added the Example of Async method

--- async.js
+++ async.js
@@ -1,14 +1,34 @@
+//Async method cannot waiting for next output...
+
+
+//Example :1(simple):
 
 setTimeout(() => {
     console.log("Hii!!!")
 }, 2000);
-
 console.log("Gauri")
-
-//Async method cannot waiting for next output...
-
 
 //output:
 //PS C:\Users\gauri\OneDrive\Desktop\node-js> node async.js
 //Gauri
 //Hii!!!
+
+
+
+//Example :1(Using function):
+
+async function fetchExample () {
+    console.log ('fetchExample')
+}
+
+async function fetchpaymentDetails () {
+    console.log ('fetchpaymentdetails')
+}
+
+fetchpaymentDetails()
+fetchExample()
+
+//output:
+////PS C:\Users\gauri\OneDrive\Desktop\node-js> node async.js
+//fetchpaymentdetails
+//fetchExample
