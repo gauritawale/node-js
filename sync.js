71537eafd74c91fd120192c7ceaf926925510ae5
gauri <gauritawale042gmail.com>
2022-11-02 10:19:16 +0530

    Added second example of sync method

--- sync.js
+++ sync.js
@@ -1,10 +1,11 @@
+//sync method can waiting for next output,Like one after another..
+
+        //Example :1(simple)
 console.log("person 1")
 console.log("person 2")
 console.log("person 3")
 console.log("person 4")
 console.log("person 5")
-
-//sync method can waiting for next output,Like one after another...
 
 //output:
 //PS C:\Users\gauri\OneDrive\Desktop\node-js> node synce.js
@@ -13,3 +14,24 @@ console.log("person 5")
 //person 3
 //person 4
 //person 5
+
+     
+
+       //Example:2(Using function)
+
+function fun1 () {
+    console.log ('function 1')
+}
+
+function fun2 () {
+    console.log ('function 2')
+}
+
+fun1()
+fun2()
+
+
+//output:
+//PS C:\Users\gauri\OneDrive\Desktop\node-js> node synce.js
+//function 1
+//function 2
